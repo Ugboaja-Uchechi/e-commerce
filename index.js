f9dfa4686db36123bf0251b9b4cc39a6d0546b8b
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-11-24 15:59:02 +0100

    Complete styling for full cart

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let cartContainer = document.querySelector('.cart-container');
+let fullContainer = document.querySelector('.full-cart-container')
 
 let emptyCart = `
 <div class="empty-cart">
@@ -7,4 +8,26 @@ let emptyCart = `
 <p>Your cart is empty</p>
 </div>`;
 
+let fullCart = `
+<div class="full-cart">
+  <h3>Cart</h3>
+  <span class="full-cart-line"></span>
+  <div class="flex">
+
+    <img class="checkout-image" src="./images/image-product-1-thumbnail.jpg" alt="snickers">
+
+    <div>
+      <h4>Autumn Limited Edition...</h4>
+      <span class="light-text">$125.00 x 3</span>
+      <span class="dark-text">$375.00</span>
+    </div>
+    <div>
+      <i class="fas fa-trash-alt trash"></i>
+    </div>
+  </div>
+  <button>Checkout</button>
+</div>
+`;
+
 cartContainer.innerHTML += emptyCart;
+fullContainer.innerHTML += fullCart;
