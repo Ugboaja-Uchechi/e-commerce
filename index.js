7495582b8dc04064245a0c068c123db6cfa51020
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-12-14 21:35:01 +0100

    Fix mobile styling bug

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let cartContainer = document.querySelector('.cart-container');
 let fullContainer = document.querySelector('.full-cart-container')
+let laptopTextContainer = document.querySelector('.laptop-flex');
 
 let emptyCart = `
 <div class="empty-cart">
@@ -16,18 +17,40 @@ let fullCart = `
 
     <img class="checkout-image" src="./images/image-product-1-thumbnail.jpg" alt="snickers">
 
-    <div>
+    <div class="mobile-view">
       <h4>Autumn Limited Edition...</h4>
       <span class="light-text">$125.00 x 3</span>
       <span class="dark-text">$375.00</span>
     </div>
-    <div>
+    <div class="mobile-view">
       <i class="fas fa-trash-alt trash"></i>
     </div>
   </div>
-  <button>Checkout</button>
+  <button class="mobile-view">Checkout</button>
 </div>
 `;
 
+let laptopText = `
+<img src="./images/image-product-1.jpg" alt="">
+<div>
+  <h3>sneaker company</h3>
+  <h2>fall limited edition Sneakers</h2>
+  <p>These low profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer</p>
+  <div class="prices">
+    <span class="amount">$125.00</span>
+    <span class="discount">50%</span>
+    <h4 class="old-price">$250.00</h4>
+  </div>
+  <div class="arithmetic">
+    <span>-</span>
+    <span>0</span>
+    <span>+</span>
+    </div>
+
+    <button class="add-button"><i class="fas fa-shopping-cart"></i> Add to cart</button>
+
+</div>`
+
+laptopTextContainer.innerHTML += laptopText
 cartContainer.innerHTML += emptyCart;
 fullContainer.innerHTML += fullCart;
