d00a0a8d1d29fe6979a44fda8985b0c363ec8ff3
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-08 19:13:40 +0100

    Create carousel interface

--- carousel.js
+++ carousel.js
@@ -1,7 +1,23 @@
 let thumbnails = document.querySelector('.thumbnails');
+let carouselContainer = document.querySelector('.carousel-container');
 
 thumbnails.addEventListener('click', () => {
   let imageCarousel = `
+  <div class="laptop-flex-cover">
+  <div class="laptop-flex-img-cover">
+    <img src="./images/image-product-1.jpg" alt="" class="laptop-flex-img z-index">
+  </div>
+  <div class="thumbnails">
+    <img src="./images/image-product-1-thumbnail.jpg" alt="">
   
+    <img src="./images/image-product-2-thumbnail.jpg" alt="">
+  
+    <img src="./images/image-product-3-thumbnail.jpg" alt="">
+  
+    <img src="./images/image-product-4-thumbnail.jpg" alt="">
+  
+  </div>
+  </div>
   `
-})
+  carouselContainer.innerHTML = imageCarousel;
+});
