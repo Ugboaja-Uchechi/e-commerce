bdf7efab632cac7734febf667be56e6a63de753f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-31 22:12:12 +0100

    Add javascript functionality

--- carousel.js
+++ carousel.js
@@ -5,6 +5,36 @@ let prev = document.querySelector('.previous-container');
 const images = document.querySelector('.carousel').children;
 const totalImages = images.length;
 
+let index = 0;
+
+prev.addEventListener('click', () => {
+  nextImage('next');
+})
+
+next.addEventListener('click', () => {
+  nextImage('prev');
+})
+
+function nextImage(direction) {
+  if(direction == 'next') {
+    index++;
+    if(index == totalImages) {
+      index = 0;
+    }
+  } else {
+    if(index == 0) {
+      index = totalImages - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for(let i = 0; i < images.length; i++) {
+    images[i].classList.remove('main');
+  }
+  images[index].classList.add('main');
+}
+
 thumbnails.addEventListener('click', () => {
   let imageCarousel = `
   <div class="laptop-flex-cover">
