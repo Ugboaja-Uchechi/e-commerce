d632c9bc04837b503ffafb46a96febb326832fec
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-31 22:11:02 +0100

    Add images and adjust styling

--- carousel.js
+++ carousel.js
@@ -1,11 +1,15 @@
 let thumbnails = document.querySelector('.thumbnails');
-let carouselContainer = document.querySelector('.carousel-container');
+let carouselContainer = document.querySelector('.carousel-container')
+
 
 thumbnails.addEventListener('click', () => {
   let imageCarousel = `
   <div class="laptop-flex-cover">
   <div class="laptop-flex-img-cover">
-    <img src="./images/image-product-1.jpg" alt="" class="laptop-flex-img z-index">
+    <img src="./images/image-product-1.jpg" alt="Sneakers" class="laptop-flex-img">
+    <img src="./images/image-product-2.jpg" alt="Sneakers" class="laptop-flex-img">
+    <img src="./images/image-product-3.jpg" alt="Sneakers" class="laptop-flex-img">
+    <img src="./images/image-product-4.jpg" alt="Sneakers" class="laptop-flex-img">
   </div>
   <div class="thumbnails">
     <img src="./images/image-product-1-thumbnail.jpg" alt="">
