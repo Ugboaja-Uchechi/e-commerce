74158cd8b646ab9ff23903e03cb21645262ec3af
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-31 23:02:37 +0100

    Fix code bug

--- cart.js
+++ cart.js
@@ -19,10 +19,8 @@ cart.addEventListener('click', () => {
   `
   cartContainer.innerHTML = emptyCartContent;
   
-  if (cartContainer.style.display === "none") {
-    cartContainer.style.display = "block";
-  } else {
-    cartContainer.style.display = "none";
+  if (cart.click) {
+    cartContainer.classList.toggle("cart-container");
   }
 });
 
